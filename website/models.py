d9fa2eb4298815ca736bdb56a5316827242dc841
Andrew Tran <andrewtran912@gmail.com>
2023-08-15 15:10:46 -0700

    Updated Todo List
    User is able to add items to the todo list, delete items from the list, and checkmark if the item is finished.

--- website/models.py
+++ website/models.py
@@ -9,10 +9,10 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
-    todolist = db.relationship('Todolist')
+    todolist = db.relationship('Task')
 
-class Todolist(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone = True), default = func.now)
+    # date = db.Column(db.DateTime(timezone = True), default = func.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- website/static/app.js
+++ website/static/app.js
@@ -25,3 +25,13 @@ document.addEventListener('DOMContentLoaded', () =>{
   }
   fetchTasks();
 });
+
+// function to delete tasks
+function deleteTask(taskId){
+  fetch('/delete-task',{
+    method: 'POST',
+    body: JSON.stringify({taskId: taskId})
+  }).then((_res) => {
+    window.location.href = '/';
+  });
+}
